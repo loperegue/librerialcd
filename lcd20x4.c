30094ff3684f0c9e56489e3907083598b8956594
loperegue <141444055+loperegue@users.noreply.github.com>
2023-08-25 16:59:07 -0300

    Add files via upload

--- lcd20x4.c
+++ lcd20x4.c
@@ -1,44 +1,94 @@
-#include "lcd20x4" 
-#include <delay.h>
-#define d0 PTB->PCR[0]
-#define d1 PTB->PCR[1]
-#define d2 PTB->PCR[2]
-#define d3 PTB->PCR[3]
-#define d4 PTB->PCR[4]
-#define d5 PTB->PCR[5]
-#define d6 PTB->PCR[6]
-#define d7 PTB->PCR[7]
-#define rs PTB->PCR[8]
-#define rw PTB->PCR[9]
-#define e PTB->PCR[10]
+ #include "delay.h"
+ #include "MKL25Z4.h"
 
 void lcd.init(){
     SIM -> SCGC5 |= SIM_SCGC5_PORTA_MASK;
     PORTA -> PCR[0] = PORT_PCR_MUX(1);
-    PORTA -> PCR[1] = PORT_PCR_MUX(2);
-    PORTA -> PCR[2] = PORT_PCR_MUX(3);
-    PORTA -> PCR[3] = PORT_PCR_MUX(4);
-    PORTA -> PCR[4] = PORT_PCR_MUX(5);
-    PORTA -> PCR[5] = PORT_PCR_MUX(6);
-    PORTA -> PCR[6] = PORT_PCR_MUX(7);
-    PORTA -> PCR[7] = PORT_PCR_MUX(8);
-    PORTA -> PCR[8] = PORT_PCR_MUX(9);
-    PORTA -> PCR[9] = PORT_PCR_MUX(10);
-    PORTA -> PCR[10] = PORT_PCR_MUX(11);
-    rs = 1;
-    rw = 1;
-    e = 0;
+    PORTA -> PCR[1] = PORT_PCR_MUX(1);
+    PORTA -> PCR[2] = PORT_PCR_MUX(1);
+    PORTA -> PCR[3] = PORT_PCR_MUX(1);
+    PORTA -> PCR[4] = PORT_PCR_MUX(1);
+    PORTA -> PCR[5] = PORT_PCR_MUX(1);
+    PORTA -> PCR[6] = PORT_PCR_MUX(1);
+    PORTA -> PCR[7] = PORT_PCR_MUX(1);
+    PORTA -> PCR[8] = PORT_PCR_MUX(1);
+    PORTA -> PCR[9] = PORT_PCR_MUX(1);
+    PORTA -> PCR[10] = PORT_PCR_MUX(1);
+    PORTA -> PCR[11] = PORT_PCR_MUX(1);
     lcd.clear();
+}
+
+void lcd.enable(){
+    PTA -> PDOR = (1<<9);
+    delay_ms(1);
+    PTA -> PCOR = (1<<9);
 }
 
 void lcd.clear(){
     PTA -> PDOR = 0x00000001;
+    lcd.enable();
     delay_ms(3);
     PTA -> PDOR = 0x00000000;
+    lcd.enable();
 }
 
-void lcd.setCursor(int fila, int col){
-    int A[4] = {00, 14, 40, 54};
-    char 
-    PTA -> PDOR = 0x00000000;
+void lcd.setCursor(int col, int fila){
+    PTA -> PDOR = 0x0000000b;
+    lcd.enable();
+    for(int i=1, i < fila, i++){
+        PTA -> PDOR = 0x0000000c;
+        lcd.enable();
+    }
+    for(int i=1, i < col, i++){
+        PTA -> PDOR = 0x0000000f;
+        lcd.enable();
+    }
+}
+
+void lcd.print(char word[20]){
+    int p = 0, n;
+    int data[8];
+    PTA -> PDOR = (1<<11);
+    while((int)(word[p]) > 0||p > 19){
+        n = (int)(word[p]);
+        for(i=0;n>0;i++)    
+        {    
+        data[i] = n % 2;    
+        n = n / 2;    
+        }
+        p++;
+        for(int i=0, i<7, i++){
+            if(data[i]){
+                PTA -> PDOR = (1<<data[i]);
+            else
+                PTA -> PCOR = (1<<data[i]);
+            }
+        }
+        lcd.enable();
+    }
+    PTA -> PCOR = (1<<11);
+}
+
+void lcd.shiftLeft(){
+    PTA -> PDOR = 0x00000018;
+    lcd.enable();
+}
+
+void lcd.shiftRight(){
+    PTA -> PDOR = 0x0000001c;
+    lcd.enable();
+}
+
+void lcd.bLightOn(){
+    PTA -> PDOR = 0x000000fe;
+    lcd.enable();
+    PTA -> PDOR = 0x00000006;
+    lcd.enable();
+}
+
+void lcd.bLightOff(){
+    PTA -> PDOR = 0x000000fe;
+    lcd.enable();
+    PTA -> PDOR = 0x00000007;
+    lcd.enable();
 }
--- lcd20x4.h
+++ lcd20x4.h
@@ -1,13 +1,14 @@
-#ifndef LCD20X4
-#define LCD20X4 
+#ifndef LCD20X4_H
+#define LCD20X4_H
 
 #include "MKL25Z4.h"
 #include "delay.h"
 
+void lcd.enable();
 void lcd.init();
 void lcd.clear();
 void lcd.setCursor(int, int);
-void lcd.print(    );
+void lcd.print(char[20]);
 void lcd.shiftLeft();
 void lcd.shiftRight();
 void lcd.bLightOn();
